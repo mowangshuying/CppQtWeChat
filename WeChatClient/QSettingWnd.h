2febc21233abcb4b03fd56459f4a02c588931521
莫忘输赢 <46436509+mowangshuying@users.noreply.github.com>
2023-04-30 10:05:31 +0800

    1.完善设置界面

--- WeChatClient/QSettingWnd.h
+++ WeChatClient/QSettingWnd.h
@@ -1,6 +1,13 @@
 #pragma once
 #include "QPch.h"
 #include <QWidget>
+#include <QHBoxLayout>
+#include <QVBoxLayout>
+#include <QPushButton>
+#include <QStackedLayout>
+#include "QSimpleSplit.h"
+#include "QSettingAboutWnd.h"
+#include <QComboBox>
 
 class QSettingWnd : public QWidget
 {
@@ -15,4 +22,24 @@ protected:
 public:
     bool m_leftBtnPressed = false;
     QPoint m_LeftBtnPressPoint;
+
+    QWidget* m_centerWnd;
+    QVBoxLayout* m_vLayout;
+
+    QHBoxLayout* m_hTitleLayout;
+    QPushButton* m_minBtn;
+    QPushButton* m_closeBtn;
+
+    //  QHBoxLayout* m_hContentLayout;
+    // QStackedLayout* m_rightLayout;
+    // QWidget* m_aboutWnd;
+
+    QHBoxLayout* m_hVerLayout;
+    QLabel* m_keyVerLabel;
+    QLabel* m_valueVerLabel;
+
+    // ÷˜Ã‚…Ë÷√
+    QHBoxLayout* m_themeLayout;
+    QLabel* m_keyThemeLabel;
+    QComboBox* m_valueThemeComboBox;
 };
