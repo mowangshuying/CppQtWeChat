59ec0e48412d956ddad5ec1fe812b0985b8ae28a
莫忘输赢 <46436509+mowangshuying@users.noreply.github.com>
2023-05-01 08:53:23 +0800

    1.[优化]删除 QDealNewFriendsApplyWnd无效代码

--- WeChatClient/QDealNewFriendsApplyWnd.cpp
+++ WeChatClient/QDealNewFriendsApplyWnd.cpp
@@ -26,21 +26,17 @@ QDealNewFriendsApplyWnd::QDealNewFriendsApplyWnd(QWidget* p /*= nullptr*/) : QWi
     m_minBtn->setIcon(QPixmap("./img/minBtn_.png"));
     m_minBtn->setIconSize(QSize(20, 20));
     m_minBtn->setFixedSize(20, 20);
-    // m_minBtn->setStyleSheet("border:0px;");
 
     m_maxBtn = new QPushButton(this);
     m_maxBtn->setIcon(QPixmap("./img/normalBtn_.png"));
     m_maxBtn->setIconSize(QSize(20, 20));
     m_maxBtn->setFixedSize(20, 20);
-    // m_maxBtn->setStyleSheet("border:0px;");
 
     m_closeBtn = new QPushButton(this);
     m_closeBtn->setIcon(QPixmap("./img/closeBtn_.png"));
     m_closeBtn->setIconSize(QSize(20, 20));
     m_closeBtn->setFixedSize(20, 20);
-    // m_closeBtn->setStyleSheet("border:0px;");
 
-    // m_hLayout->addSpacing(20);
     m_hLayout->addStretch();
     m_hLayout->setSpacing(5);
     m_hLayout->addWidget(m_minBtn);
@@ -48,7 +44,6 @@ QDealNewFriendsApplyWnd::QDealNewFriendsApplyWnd(QWidget* p /*= nullptr*/) : QWi
     m_hLayout->addWidget(m_closeBtn);
     m_vLayout->addLayout(m_hLayout);
 
-    // m_sLayout = new QStackedLayout();
     m_listWnd1 = new QListWidget();
     m_listWnd2 = new QListWidget();
 
@@ -67,13 +62,6 @@ QDealNewFriendsApplyWnd::QDealNewFriendsApplyWnd(QWidget* p /*= nullptr*/) : QWi
     m_listWnd1->setAttribute(Qt::WA_StyledBackground);
     m_listWnd2->setAttribute(Qt::WA_StyledBackground);
     setAttribute(Qt::WA_StyledBackground);
-
-    // m_listWnd1->setStyleSheet("border:0px;");
-    // m_listWnd2->setStyleSheet("border:0px;");
-
-    if (objectName().isEmpty())
-        setObjectName("QDealNewFriendsApplyWnd");
-    // setStyleSheet("QWidget#QDealNewFriendsApplyWnd{border-top-right-radius:4px;border-bottom-right-radius:4px; background-color:white;border:none;}");
 }
 
 void QDealNewFriendsApplyWnd::resizeEvent(QResizeEvent* event)
